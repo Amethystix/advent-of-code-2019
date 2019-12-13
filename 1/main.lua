b84266d48b5feb9bd5fbe300a78a08d8d9b7a25f
lad475 <lauren.a.digiovanni@weightwatchers.com>
2019-12-13 10:28:15 -0500

    Part two solved

--- 1/main.lua
+++ 1/main.lua
@@ -19,6 +19,23 @@ function calcTotal(input)
   return result
 end
 
+-- Part 1
+
 local input = inputToArray()
 print(calcTotal(input))
 
+-- Part 2
+
+function calcPartTwoTotal(input)
+  result = 0
+  for k, v in ipairs(input) do
+    local currentResult = calcFuel(v)
+    while (currentResult > 0) do
+      result = result + currentResult
+      currentResult = calcFuel(currentResult)
+    end
+  end
+  return result
+end
+
+print(calcPartTwoTotal(input))
