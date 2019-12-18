e6af66ceeecac344530ad87a133291d3ab2c0e32
lad475 <lauren.a.digiovanni@weightwatchers.com>
2019-12-17 19:33:08 -0500

    Day 4 part 2

--- 4/main.lua
+++ 4/main.lua
@@ -1,17 +1,33 @@
 
+-- Updated for part 2
 function isValid(current)
   local currentTbl = numberToTable(current)
   local prevDigit = currentTbl[1]
   local hasAdjacent = false
+  local current2Match = false
+  local bigGroup = false
   for place, digit in ipairs(currentTbl) do
     if (place ~= 1) then
       if (digit < prevDigit) then 
         return 0
       elseif (digit == prevDigit) then
-        hasAdjacent = true
+        if (current2Match == true) then
+          bigGroup = true
+        else
+          current2Match = true
+        end
+      elseif (digit ~= prevDigit) then
+        if (current2Match == true and bigGroup ~= true) then
+          hasAdjacent = true
+        end
+        current2Match = false
+        bigGroup = false
       end
     end
     prevDigit = digit
+  end
+  if (current2Match == true and bigGroup ~= true) then
+    hasAdjacent = true
   end
   if (hasAdjacent) then
     return 1
